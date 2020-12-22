5fa56b9afe9fc73f784daf7b350676352bac76ef
”Oklevet” <nikstar1235@gmail.com>
2020-12-22 09:10:24 +0500

    2. Сокобан. Тестовое задание. [#430165]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,42 +2,31 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        return checkHorizontal(board) || checkVertical(board);
-    }
-
-    public static boolean checkHorizontal(int[][] board) {
-        int count = 0;
-        for (int[] i : board) {
-            for (int j : i) {
-                if (j == 0) {
-                    count = 0;
-                    break;
-                } else {
-                    count++;
-                }
-            }
-            if (count == board.length) {
-                return true;
-            }
-        }
-        return count == board.length;
-    }
-
-    public static boolean checkVertical(int[][] board) {
-        int count = 0;
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[j][i] == 0) {
-                    count = 0;
-                    break;
-                } else {
-                    count++;
+            if (board[i][i] == 1) {
+                if (checkHorizontal(board, i) || checkVertical(board, i)) {
+                    return true;
                 }
             }
-            if (count == board.length) {
-                return true;
+        }
+        return false;
+    }
+
+    public static boolean checkHorizontal(int[][] board, int row) {
+        for (int i : board[row]) {
+            if (i != 1) {
+                return false;
             }
         }
-        return count == board.length;
+        return true;
+    }
+
+    public static boolean checkVertical(int[][] board, int column) {
+        for (int[] i : board) {
+            if (i[column] != 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
